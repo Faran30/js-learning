a308840500a65959caf41834ec7c67941f262f53
Faran30 <kfaran30o1@gmail.com>
2025-09-08 21:13:24 +0000

    upgrade

---
+++ basics/datatypes.js
@@ -0,0 +1,8 @@
+let a1 ="faran"  //string
+let a2 = 99      //number
+let a3 = 9.99    //number
+let a4 = true    //bolean
+let a5;          // undefined
+let a6 = null    //null 
+
+console.table([a1, a2, a3, a4, a5, a6])
--- basics/variable.js
+++ basics/variable.js
@@ -2,4 +2,9 @@ const userId = 70180959
 let userName = "faran"
 var userlove = "ahmad"
 
+    // userId = 701701 cant possible
+    userName = "tayyab"
+
+
 console.table([userId, userName, userlove])
+

7dee325aeb404268427356cbc102e58edff2c65a
Faran30 <kfaran30o1@gmail.com>
2025-09-10 14:26:20 +0000

    convertin

---
+++ basics/conversionoperation.js
@@ -0,0 +1,29 @@
+let score = "33"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueinnumber = Number(score)
+//console.log(typeof (valueinnumber))
+//console.log(valueinnumber)
+
+
+let islogedin = "faran"
+
+
+let boleanislogedin = Boolean(islogedin)
+//console.log(boleanislogedin)
+
+let value = 33
+
+let stringinvalue = String(value)
+
+console.log(typeof stringinvalue)
+
+
+//in this program we have done convertion 
+//we have converted variable types like 
+//we change number to string number or string to bollean
+// and we have number to string and and string to number 
+
+// ************************************operations*************************************************

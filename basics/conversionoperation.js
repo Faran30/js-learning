5e05273fbaba1abf5193cc0985ffec8a0918567a
Faran30 <kfaran30o1@gmail.com>
2025-09-14 14:55:27 +0000

    comperresion

--- basics/conversionoperation.js
+++ basics/conversionoperation.js
@@ -1,24 +1,24 @@
-let score = "33"
+// let score = "33"
 
-//console.log(typeof score)
-//console.log(typeof(score))
+// //console.log(typeof score)
+// //console.log(typeof(score))
 
-let valueinnumber = Number(score)
-//console.log(typeof (valueinnumber))
-//console.log(valueinnumber)
-
-
-let islogedin = "faran"
+// let valueinnumber = Number(score)
+// //console.log(typeof (valueinnumber))
+// //console.log(valueinnumber)
 
 
-let boleanislogedin = Boolean(islogedin)
-//console.log(boleanislogedin)
+// let islogedin = "faran"
 
-let value = 33
 
-let stringinvalue = String(value)
+// let boleanislogedin = Boolean(islogedin)
+// //console.log(boleanislogedin)
 
-console.log(typeof stringinvalue)
+// let somenumber = 33
+
+// let stringinsomenumber = String(somenumber)
+
+// console.log(typeof stringinsomenumber)
 
 
 //in this program we have done convertion 
@@ -27,3 +27,19 @@ console.log(typeof stringinvalue)
 // and we have number to string and and string to number 
 
 // ************************************operations*************************************************
+
+let value = 5
+let positivevalue = +value
+// console.log(positivevalue)
+
+// console.log(++value)
+// console.log(4 + 5)
+// console.log(2 * 5)
+// console.log(8 % 4)
+
+let gamecounter = 100
+console.log (gamecounter)
+let adgamecounter = gamecounter++
+
+console.log (adgamecounter)
+console.log (gamecounter)

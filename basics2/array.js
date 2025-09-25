4b27fa75f1c96030cacb133b9f0065c2ca8131ac
Faran30 <kfaran30o1@gmail.com>
2025-09-25 07:56:07 +0000

    array

---
+++ basics2/array.js
@@ -0,0 +1,21 @@
+const arr = [1, 2, 3, 4, 5];
+
+
+
+// console.log(arr);
+
+console.log("A ", arr);
+const myarr1 = arr.slice(1, 3)
+console.log(arr);
+console.log(myarr1);
+
+
+console.log("B ", arr);
+const myarr2 = arr.splice(1, 3)
+console.log(arr);
+console.log(myarr2);
+
+
+
+
+

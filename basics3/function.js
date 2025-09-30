975506ef5123139b566de67b698d16a06d7e833b
Faran30 <kfaran30o1@gmail.com>
2025-09-30 22:29:13 +0000

    function1

---
+++ basics3/function.js
@@ -0,0 +1,15 @@
+function addtwonumber(mumber1 , number2){
+
+mumber1 + number2
+}
+addtwonumber(1 , 2)
+
+function isloggedin(username){
+    if(username === undefined){
+        console.log("please Enter your name");
+        return
+    }
+    
+    return `${username} is logged in`
+}
+ console.log(isloggedin());

bf896938f0a69291c496f3f1c348d361ad4e964c
Faran30 <kfaran30o1@gmail.com>
2025-10-02 20:34:57 +0000

    scope

--- basics3/function.js
+++ basics3/function.js
@@ -12,4 +12,26 @@ function isloggedin(username){
     
     return `${username} is logged in`
 }
- console.log(isloggedin());
+//  console.log(isloggedin());
+
+function addtomycart(...num1){
+    return num1
+}
+// console.log(addtomycart(4, 4, 5));
+
+const user = {
+    username :"faran",
+    price : 999,
+}
+const user1 = {
+    username : "abdullah",
+    price : 500,
+}
+function myownfunction (anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+myownfunction(user)
+
+
+
+
---
+++ basics3/scope.js
@@ -0,0 +1,12 @@
+let a = 30 
+const b = 40
+
+if (true) {
+    let a = 10 
+const b = 20 
+console.log(a);
+console.log(b);
+
+}
+console.log(a);
+console.log(b);

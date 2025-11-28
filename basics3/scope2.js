d0a8b85c889006e7cdb717f295e67a203895582e
Faran30 <kfaran30o1@gmail.com>
2025-11-28 19:06:48 +0000

    .....

---
+++ basics3/scope2.js
@@ -0,0 +1,25 @@
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+const addtwo = function(num){
+    return num + 2
+}
+
+console.log(addtwo(4))
+
+
+function fun1(){
+    const name = "faran"
+console.log(name)
+
+    function fun2(){
+        const website = "vscode"
+        console.log(website)
+    }
+    
+  console.log(fun1()) 
+}
+ 
